7c51a039ede3c7787f7bfea2c6cbcb6755a80348
RoseTame <2077225170@qq.com>
2024-05-03 08:47:33 +0800

    Icon commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.exoplayer)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
